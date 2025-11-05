9f1dfdb02278b3d9f8cedbf56e22d86527b892f9
Rachit Mudgil <118411741+Reo96@users.noreply.github.com>
2025-11-05 11:51:04 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Reo96",
   resume:
-    "https://drive.google.com/file/d/1XH1fqDLEMdrc2JsDxfUjiwDQY7p6IVZN/view?usp=sharing",
+    "https://drive.google.com/file/d/1qjAVeeqz-r4uIBvmLuT_fsuNMexBps58/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/rachit-mudgil-93329927b/",
   twitter: "https://www.instagram.com/rachit_mudgil/",//
   insta: "https://www.instagram.com/rachit_mudgil/",
@@ -377,5 +377,6 @@ export const projects = [
     webapp: "https://todos-0a2w.onrender.com/",
   },
 ];
+
 
 
